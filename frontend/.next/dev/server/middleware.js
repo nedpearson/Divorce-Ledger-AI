c38bb776cee176c4121a9f26ced0ccdb6d195bd6
nedpearson <nedpearson@gmail.com>
2026-02-22 21:31:28 -0600

    fix: remove build artifacts

--- frontend/.next/dev/server/middleware.js
+++
@@ -1,7 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/b55fd_next_00c89ffd._.js")
-R.c("server/chunks/b55fd_@supabase_auth-js_dist_module_b2789e26._.js")
-R.c("server/chunks/b55fd_3fefccee._.js")
-R.c("server/chunks/[root-of-the-server]__6588b603._.js")
-R.m("[project]/Desktop/Divorce-Ledger-AI/frontend/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/Desktop/Divorce-Ledger-AI/frontend/src/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)")
-module.exports=R.m("[project]/Desktop/Divorce-Ledger-AI/frontend/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/Desktop/Divorce-Ledger-AI/frontend/src/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)").exports
--- frontend/.next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/b55fd_next_69a8d72d._.js")
-R.c("server/chunks/ssr/[externals]__e6a4d965._.js")
-R.m("[project]/Desktop/Divorce-Ledger-AI/frontend/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/Desktop/Divorce-Ledger-AI/frontend/node_modules/next/document.js [ssr] (ecmascript)").exports
